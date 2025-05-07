ad5744281729801aec891227e3632c1dbb868ae0
Matheus Reis <matheus201922@hotmail.com>
2025-05-07 01:00:43 -0300

    adding logout function

--- Customer Registration/src/Components/Navbar.jsx
+++ Customer Registration/src/Components/Navbar.jsx
@@ -5,6 +5,8 @@ import { Menu } from "lucide-react"
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import { X } from 'lucide-react';
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/firebase";
 
 const Navbar = ({ searchQuery, handleSearch}) => {
     const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
@@ -26,8 +28,16 @@ const Navbar = ({ searchQuery, handleSearch}) => {
   
   const navigate = useNavigate();
   const [sideBar, setSideBar] = useState(false)
-  const handleLogoff = () => {
-    navigate("/");
+
+  const handleLogoff = async () => {
+    try {
+
+      await signOut(auth);
+      navigate("/");
+
+    } catch (error) {
+      console.error("Erro ao deslogar:", error);
+    }
   };
 
   const handleSidebar = () => {
