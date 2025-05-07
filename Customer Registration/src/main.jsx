edf9058a8ab44a0dba3e6ca977fe37c29f1ce858
Matheus Reis <matheus201922@hotmail.com>
2025-05-07 00:17:59 -0300

    protecting project's pages

--- Customer Registration/src/main.jsx
+++ Customer Registration/src/main.jsx
@@ -6,16 +6,31 @@ import App from './App.jsx'
 import ClientList from './pages/ClientList.jsx'
 import LoginScreen from './pages/LoginScreen.jsx'
 import Scheduling from './pages/Scheduling.jsx'
+import PrivateRoute from './route/PrivateRoute.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoginScreen/>}/>
-        <Route path='/client-list' element={<ClientList/>}/>
-        <Route path='/scheduling' element ={<Scheduling/>}/>
+      <Routes>  
+        <Route path='/' element={<LoginScreen />} />      
+        <Route
+          path='/client-list'
+          element={
+            <PrivateRoute>
+              <ClientList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/scheduling'
+          element={
+            <PrivateRoute>
+              <Scheduling />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
